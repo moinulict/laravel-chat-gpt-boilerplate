40d163e38abffbda8939e12b83f095e0e9a33470
minulsit <minul@sundarbanit.com.bd>
2023-07-18 17:33:22 +0600

    api update

--- app/Http/Controllers/ChatGPTController.php
+++ app/Http/Controllers/ChatGPTController.php
@@ -22,13 +22,7 @@ class ChatGPTController extends Controller
                             "role" => "user",
                             "content" => $request->message
                         ]
-                    ],
-                    'temperature' => 0.5,
-                    "max_tokens" => 200,
-                    "top_p" => 1.0,
-                    "frequency_penalty" => 0.52,
-                    "presence_penalty" => 0.5,
-                    "stop" => ["11."],
+                    ]
                 ])
                 ->json();
 
